9ea6069ae7c33b07ed0bf5f0b4d265400ea1934e
infsolution <clsinfsolution@gmail.com>
2019-03-16 12:19:38 -0300

    Add folder of project init

---
+++ 00-introducaots/app/ts/app.js
@@ -0,0 +1,15 @@
+var nome = "Type Script";
+console.log("Hello, " + nome);
+var numero = 10;
+console.log(numero);
+var letra = "Cicero é foda";
+console.log(letra);
+var bin = 585;
+var exad = 0xfad43;
+var octa = 14419;
+var real = 2.564654;
+console.log(bin);
+console.log(exad);
+console.log(octa);
+console.log(real);
+//# sourceMappingURL=app.js.map
---
+++ 00-introducaots/app/ts/app.ts
@@ -0,0 +1,15 @@
+let nome : string = "Type Script"
+console.log("Hello, " + nome);
+let numero : number = 10;
+console.log(numero);
+let letra : string = "Cicero é foda";
+console.log(letra);
+
+let bin : number = 0b1001001001;
+let exad : number = 0xfad43;
+let octa : number = 0o34123;
+let real : number = 2.564654;
+console.log(bin);
+console.log(exad);
+console.log(octa);
+console.log(real);
